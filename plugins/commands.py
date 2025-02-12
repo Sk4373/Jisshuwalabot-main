6d793d06572227d2bd7e2abd641d81968670ee10
Sadique Barlaskar <129667908+Sk4373@users.noreply.github.com>
2025-02-12 20:15:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -65,8 +65,10 @@ async def start(client:Client, message):
         if await db.user_verified(user_id):
              key = "third_time_verified"
         else:
-            key = "second_time_verified" if 
-        await db.is_user_verified(user_id) else "first_time_verified"
+        if await db.is_user_verified(user_id):
+             key = "second_time_verified"
+         else:
+             key = "last_verified"
 
 current_time = dt.now(tz=ist_timezone)  # Yeh line properly aligned hai
 
