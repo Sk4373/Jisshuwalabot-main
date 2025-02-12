b138b548af32359bc375336ac80e76d7548207c4
Sadique Barlaskar <129667908+Sk4373@users.noreply.github.com>
2025-02-12 19:41:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -63,12 +63,9 @@ async def start(client:Client, message):
         ist_timezone = pytz.timezone('Asia/Kolkata')
         
         if await db.user_verified(user_id):
-           key = "third_time_verified"
-        else:
-            if await db.is_user_verified(user_id):
-                           key = "second_time_verified"
-                       else:
-                          key = "last_verified"
+    key = "third_time_verified"
+else:
+    key = "second_time_verified" if await db.is_user_verified(user_id) else "last_verified"
 
 current_time = dt.now(tz=ist_timezone)  # Yeh line properly aligned hai
 
